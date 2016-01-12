5d181fc66546098fd5dcfa3fa08e0c2216e47ef3
Claudia Mane <c@luperlabs.com>
2016-01-12 01:35:30 -0800

    Plots object accepts a matrix of plants and calculates the scores.

--- garden.py
+++ garden.py
@@ -4,12 +4,28 @@ import json
 
 PLANT_FILE_JSON = 'plants.json'
 
-
 def get_plant_data():
+    """
+    Load JSON data from plants.
+    Map plant values to each key.
+    Friendly plants get +10 points, harmful plants get -10 points, and similar plants
+    get -5 points.
+    """
     with open(PLANT_FILE_JSON, 'r') as plantfile:
         plants = json.load(plantfile)
-    return plants
 
+    plant_values = {}
+    for plant, values in plants.items():
+        plant_values[plant] = {plant: -5}
+        plant_values[plant].update(
+            {friend:10 for friend in values['friends']}
+        )
+        plant_values[plant].update(
+            {foe:-10 for foe in values['foes']}
+        )
+    return plant_values
+
+PLANT_VALUES = get_plant_data()
 
 class TooManyPlants(Exception):
     pass
@@ -19,8 +35,104 @@ class PlantDoesNotExist(Exception):
     pass
 
 
+class PlotNotSet(Exception):
+    pass
+
+
+class NotValidCoordinate(Exception):
+    pass
+
+class InvalidPlot(Exception):
+    pass
+
+
+class Plots(object):
+    """
+    Each plot is a matrix of nested arrays.
+    X value is associated with rows, Y value is associated with columns.
+    Coordinates follow:
+                    y (col)
+                (0, 0)      (0, y+n)
+        x (row)
+                (x+n, 0)    (x+n, y+n)
+    Plots must be rectangular.
+    """
+    PlantValues = PLANT_VALUES
+
+    def __init__(self, plot=None):
+        self.plot = plot
+        self.set_plots(plot)
+
+    def update_plot_dimensions(self):
+        """
+        Check that the plot is valid dimensions, and update the row and column count.
+        """
+        if not self.plot:
+            self.rows = 0
+            self.columns = 0
+            return
+
+        # Check to make sure all the rows are the same length.
+        if not all(len(self.plot[0]) == len(row) for row in self.plot):
+            raise InvalidPlot("Plot rows must be the same size.")
+        self.rows = len(self.plot)
+        self.columns = len(self.plot[0])
+
+    def set_plots(self, plot):
+        """
+        Set or update plot matrix.
+        Ex. [
+            ['apple', 'pear'],
+            ['apple', 'apple'],
+            ['pear', 'pear']
+            ]
+        """
+        self.plot = plot
+        self.update_plot_dimensions()
+
+    def check_coordinates(self, x, y):
+        if x < 0 or y < 0:
+            raise NotValidCoordinate("Coordinates must be positive integers.")
+        if x >= self.rows:
+            raise NotValidCoordinate("X-axis exceeds plot width.")
+        if y >= self.columns:
+            raise NotValidCoordinate("Y-axis exceeds plot length.")
+
+    def _get_neighbors(self, x, y):
+        top_row = max(x-1, 0)
+        start_column = max(y-1, 0)
+
+        for row in range(top_row, x+1):
+            for column in range(start_column, y+1):
+                if row != x and column != y:
+                    yield self.plot[row][column]
+
+    def get_plant(self, x, y):
+        """
+        Return plant type for (x, y) coordinate pair.
+        """
+        self.check_coordinates(x, y)
+        return self.plot[x][y]
+
+    def get_plot_score(self, x, y):
+        self.check_coordinates(x, y)
+        score = 0
+        neighbors = list(self._get_neighbors(x, y))
+        current_plant = self.get_plant(x, y)
+        for plant in neighbors:
+            score += self.PlantValues.get(plant, {}).get(current_plant, 0)
+        return score
+
+    def get_total_score(self):
+        score = 0
+        for x in range(self.rows):
+            for y in range(self.columns):
+                score += self.get_plot_score(x, y)
+        return score
+
+
 class Garden(object):
-    PlantValues = get_plant_data()
+    PlantValues = PLANT_VALUES
 
     def __init__(self, length=1, width=1):
         """
@@ -32,6 +144,12 @@ class Garden(object):
         self.plot_size = length * width
         self.plants = {}
         self.plants_num = 0
+        self.plot = Plots()
+
+    def score_plot():
+        score = 0
+
+
 
     def clean_plant(self, plant_string):
         """Normalizes plant name and checks if plant values exist."""
--- tests.py
+++ tests.py
@@ -1,12 +1,12 @@
 import unittest
-from garden import Garden, TooManyPlants, PlantDoesNotExist
+import garden
 
 class TestGarden(unittest.TestCase):
 
     def setUp(self):
         self.length = 5
         self.width = 6
-        self.garden = Garden(self.length, self.width)
+        self.garden = garden.Garden(self.length, self.width)
 
     def test_plot_size(self):
         """Plot size should equal length * width."""
@@ -26,12 +26,41 @@ class TestGarden(unittest.TestCase):
         self.assertEqual(self.garden.plants_num, 30)
 
     def test_plant_does_not_exist(self):
-        self.assertRaises(PlantDoesNotExist, self.garden.add, 'kitty-cat')
+        self.assertRaises(garden.PlantDoesNotExist, self.garden.add, 'kitty-cat')
 
     def test_too_many_plants(self):
         for i in range(30):
             self.garden.add('leeks')
-        self.assertRaises(TooManyPlants, self.garden.add, 'leeks')
+        self.assertRaises(garden.TooManyPlants, self.garden.add, 'leeks')
 
 
+class TestPlots(unittest.TestCase):
+
+    def setUp(self):
+        garden_plot = [
+            ['corn', 'corn', 'beans', 'beans'],
+            ['corn', 'corn', 'beans', 'corn'],
+            ['corn', 'lettuce', 'beans', 'corn'],
+            ['corn', 'corn', 'corn', 'garlic'],
+        ]
+        self.plot = garden.Plots()
+        self.plot.set_plots(garden_plot)
+
+    def test_plot_dimentions(self):
+        self.assertEqual(self.plot.rows, 4)
+        self.assertEqual(self.plot.columns, 4)
+
+    def test_coordinate_validity(self):
+        self.assertRaises(garden.NotValidCoordinate, self.plot.get_plant, 8, 4)
+        self.assertRaises(garden.NotValidCoordinate, self.plot.get_plant, 3, -4)
+        self.assertRaises(garden.NotValidCoordinate, self.plot.get_plant, 3, 10)
+
+    def test_get_plot(self):
+        self.assertEqual(self.plot.get_plant(3, 3), 'garlic')
+
+    def test_get_plot_score(self):
+        self.assertEqual(self.plot.get_plot_score(3, 3), -10)
+
+    def test_get_total_score(self):
+        self.assertEqual(self.plot.get_total_score(), 20)
 
